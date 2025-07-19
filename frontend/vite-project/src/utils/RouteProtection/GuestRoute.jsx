5253f24336262b34ed7ceae43a623d3709948193
Nirajan09 <nirajantiwari09@gmail.com>
2025-07-19 13:40:58 +0545

    Integration of Admin Dashboard

--- frontend/vite-project/src/utils/RouteProtection/GuestRoute.jsx
+++ frontend/vite-project/src/utils/RouteProtection/GuestRoute.jsx
@@ -1,12 +1,22 @@
-// src/components/GuestRoute.js
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../../utils/AuthContext/AuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../AuthContext/AuthContext";
 
 export default function GuestRoute({ children }) {
-  const { token } = useAuth();
+  const { token, user } = useAuth();
+  const navigate = useNavigate();
 
-  if (token) {
-    return <Navigate to="/dashboard" replace />;
-  }
+  useEffect(() => {
+    if (token && user) {
+      if (user.is_staff) {
+        navigate("/admin", { replace: true });
+      } else {
+        navigate("/dashboard", { replace: true });
+      }
+    }
+  }, [token, user, navigate]);
+
+  if (token && user) return null;
+
   return children;
 }
