7d94d868593fd880611cd588a992371dbd3e2514
Nirajan09 <nirajantiwari09@gmail.com>
2025-08-07 13:05:57 +0545

    Added the functionality of remove cart after the order is place

--- backend/bookhub/orders/views.py
+++ backend/bookhub/orders/views.py
@@ -1,21 +1,27 @@
 from rest_framework import viewsets, permissions
 from .models import Order, OrderItem
 from .serializers import OrderSerializer, OrderItemSerializer
+from books.models import CartItem
 
 class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # Admin/staff see all orders; customers see only theirs
         user = self.request.user
         if user.is_staff:
             return Order.objects.all()
         return Order.objects.filter(user=user)
 
     def perform_create(self, serializer):
-        # Automatically assign the logged-in user to the order on creation
-        serializer.save(user=self.request.user)
+        # Save the order and assign user
+        order = serializer.save(user=self.request.user)
+
+        # Collect all book IDs from the order items
+        ordered_book_ids = order.items.values_list('book__id', flat=True)
+
+        # Delete cart items that belong to the user and match the ordered books
+        CartItem.objects.filter(user=self.request.user, book__id__in=ordered_book_ids).delete()
 
 
 class OrderItemViewSet(viewsets.ReadOnlyModelViewSet):
