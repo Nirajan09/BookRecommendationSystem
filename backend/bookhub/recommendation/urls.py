4f1f91e97bd68d8d33bd0c0d98eacaaaa7aecd2b
Nirajan09 <nirajantiwari09@gmail.com>
2025-11-19 12:20:04 +0545

    Added postgres to the railway

--- backend/bookhub/recommendation/urls.py
+++ backend/bookhub/recommendation/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from .views import evaluate
+# from .views import evaluate
 
 urlpatterns = [
-    path("recommend/", views.recommend_books, name="recommend_books"),
-    path("evaluate/", evaluate, name="evaluate"),    
+    # path("recommend/", views.recommend_books, name="recommend_books"),
+    # path("evaluate/", evaluate, name="evaluate"),    
 ]
